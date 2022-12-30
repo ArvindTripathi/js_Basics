e4c90c0235796e2224552426b4edb277608aa7a8
ArvindTripathi <37835353+ArvindTripathi@users.noreply.github.com>
2022-12-30 20:48:00 +0000

    Conversion 0f datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "Aman"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true =>1 ; false => 0
+
+let flag = "AMan"
+
+let booleanflag = Boolean(flag)
+
+console.log(booleanflag)
+
+// "" ==> false
+// "Aman" ==. true
+// 1==> true ; 0==> false
+
